ab6ed9056a54c7fe87c9e2fc53c4a69d3c680765
Avioflagos <120939905+AviOfLagos@users.noreply.github.com>
2026-08-13 07:58:41 +0100

    Portfolio v1

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import '@fontsource-variable/space-grotesk'
+import '@fontsource/jetbrains-mono/400.css'
+import '@fontsource/jetbrains-mono/500.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
